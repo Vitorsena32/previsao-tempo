cbacbf090230391799522d37563033e40191e639
vitorsena32 <vitorsena32@gmail.com>
2025-01-02 13:17:13 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+
+let chave = "e62da9726f9732ba6583cf58816315f7"
+
+
+function dateInScreen(dados){
+    console.log(dados)
+    document.querySelector(".cidade").innerHTML = "Tempo em " + (dados.name)
+    document.querySelector(".temp").innerHTML =  Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = (dados.main.humidity) + "% umidade do ar"
+    document.querySelector(".img-previsao").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+}
+
+async function searchCity(city){
+    let date = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + 
+    city + 
+    "&appid=" + 
+    chave + 
+    "&lang=pt_br" +
+    "&units=metric"
+    )
+    .then(response => response.json())
+
+    dateInScreen(date)
+}
+
+
+function clickInButton(){
+   let city = document.querySelector(".input-cidade").value
+
+   searchCity(city)
+}
